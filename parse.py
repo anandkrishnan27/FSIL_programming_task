6d84f6271826ec7868903bd2f9e7cf9d70c22646
anandkrishnan27 <166864805+anandkrishnan27@users.noreply.github.com>
2024-04-13 15:47:35 -0700

    Update parse.py
    works for most files, still a little buggy for extracting item 7a.

--- parse.py
+++ parse.py
@@ -3,40 +3,45 @@ import downloader
 import os
 
 company_name = downloader.get_company()
-base_path = "/Users/anandkrishnan/Desktop/FSIL project/sec-edgar-filings/" + company_name
+base_path = "/Users/anandkrishnan/Desktop/FSIL project/sec-edgar-filings/" + company_name + "/10-K"
+statement_number = 0
+
+def find_nth(input, substring, n):
+    start = input.find(substring)
+    while start >= 0 and n > 1:
+        start = input.find(substring, start + 1)
+        n -= 1
+    return start
 
 for root, dirs, files in os.walk(base_path):
-    for file in files:
-        # Construct the full path to the file
-        file_path = os.path.join(root, file)
-        
-        # Perform your operation on the file
-        # For example, you can open and read the file
-        with open(file_path, 'r') as f:
-            # Perform your operation here
-            # For example:
-            file_contents = f.read()
-            # Do something with the file contents
+    for directory in dirs:
+        # Process each subfolder
+        file_path = os.path.join(root, directory)
+        print(file_path)
+        with open(file_path + "/full-submission.txt", 'r') as f:
+            
+            # parse contents into a string
+            contents = f.read()
+            soup = BeautifulSoup(contents, "html.parser")
+            body_text = soup.body.get_text(separator= ' ')
 
-with open("/Users/anandkrishnan/Desktop/FSIL project/sec-edgar-filings/NVDA/10-K/0001012870-02-002262/full-submission.txt", "r", encoding="utf-8") as f:
-    html_content = f.read()
+            # parsing data and extracting only selected financial statements from the 10-K (assumes they are always Item 6.)
+            to_remove = ['\n', '\u00A0', ' '] # removing various characters that will interfere with string.find() method
+            for char in to_remove:
+                body_text = body_text.replace(char, " ")
+            body_text = body_text.lower()
 
-# Parse HTML content
-soup = BeautifulSoup(html_content, "html.parser")
+            start = find_nth(body_text, "item 7a.", 2)
+            end = find_nth(body_text, "item 8.", 2)
 
-# Extract text from body tag
-body_text = soup.body.get_text()
+            print(start)
+            print(end)
+            body_text = body_text[start : end]
 
-# parsing data and extracting only selected financial statements from the 10-K (assumes they are always Item 6.)
-body_text = body_text.replace("\n", "")
-start = body_text.find("ITEM 6.")
-end = body_text.find("ITEM 7.")
-body_text = body_text[start : end]
+            file = open("/Users/anandkrishnan/Desktop/FSIL project/sec-edgar-filings/financial_statements/" + company_name + str(statement_number) + ".txt", "w")
+            file.write(body_text)
+            file.close()
 
-file = open("/Users/anandkrishnan/Desktop/FSIL project/sec-edgar-filings/parsed_data1.txt", "w")
-file.write(body_text)
-file.close()
-
-# selected financial data is what we need
-# items 1A and 6
- 
+        statement_number += 1
+# item 7a (which we are extracting) contains exposure to market risk data -> investors might want to know this
+# depending on their tolerance for exposure to macroeconomic conditions/risk (e.g., NVDA investing in MBS in 2006, etc.)
