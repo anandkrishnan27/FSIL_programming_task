5a216d5a0a9517d212d4e121f2d030a2f691cc6e
anandkrishnan27 <166864805+anandkrishnan27@users.noreply.github.com>
2024-04-18 11:15:18 -0700

    Update api_calls.py
    updated structure to remove raw code and replace with only functions called in another file

--- api_calls.py
+++ api_calls.py
@@ -3,9 +3,6 @@ import parse
 from openai import OpenAI
 from dotenv import load_dotenv
 
-good_filings = parse.get_filings()
-company = downloader.get_company()
-
 def pick_years(filings, num_to_pick):
     final_list = []
     if len(filings) <= 0: return final_list
@@ -18,8 +15,6 @@ def pick_years(filings, num_to_pick):
     
     final_list.append(filings[len(filings) - 1])
     return final_list
-
-filings_to_use = pick_years(good_filings, 7)
 
 def generate_prompt(filings):
     prompt = ""
@@ -35,44 +30,41 @@ def generate_prompt(filings):
     
     return prompt
 
-filing_data = generate_prompt(filings_to_use)
-
-load_dotenv()
-client = OpenAI(api_key="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") # resolve how to store api key
-
-primer = f"""You are a very confident, knowledgeable market analyst who can concisely analyze market risk factors affecting 
-a company. Parse the below excerpts from {company}'s 10-K item 7a and answer the following questions. Item 7a over 
-time for {company} is below.""" + '\n\n' + filing_data
-
 results = []
-questions = ["What are some possible macroeconomic factors that pose risk to this firm?"
-             "Give a future outlook for this firm's risk profile", 
-             "How risky are this firm's investments historically?", 
-             "How should this company improve in order to lessen its risk profile?", 
-             "Give me some matplotlib code that generates graphs with some visualization for this company."]
 
-# priming message
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": primer},
-    ]
-)
+def run_api(company, filing_data):
+    load_dotenv()
+    client = OpenAI(api_key="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") # resolve how to store api key
 
-for question in questions:
+    primer = f"""You are a very confident, knowledgeable market analyst who can concisely analyze market risk factors affecting 
+    a company. Parse the below excerpts from {company}'s 10-K item 7a and answer the following questions. Item 7a over 
+    time for {company} is below.""" + '\n\n' + filing_data
+
+    questions = ["What are some possible macroeconomic factors that pose risk to this firm?"
+                "Give a future outlook for this firm's risk profile", 
+                "How risky are this firm's investments historically?", 
+                "How should this company improve in order to lessen its risk profile?", 
+                "Give me some matplotlib code that generates graphs with some visualization for this company."]
+
+    # priming message
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": question}
+            {"role": "system", "content": primer},
         ]
-    )   
+    )
 
-    results.append(completion.choices[0].message)
+    for question in questions:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": question}
+            ]
+        )   
 
-def get_results():
+        results.append(completion.choices[0].message)
+        
     return results
 
 # should generate all text differently, so 5 different API calls to make a list with 5 elements that will be 
 # passed into the app.py file
-
-
