0f334d36ffbc646087738af4ecb85958b9ac51eb
anandkrishnan27 <166864805+anandkrishnan27@users.noreply.github.com>
2024-04-18 11:13:01 -0700

    Update downloader.py
    updated structure to be functions, not raw code in order for app.py to work properly

--- downloader.py
+++ downloader.py
@@ -1,9 +1,16 @@
 from sec_edgar_downloader import Downloader
 
 AFTER_DATE = "1995-01-01"
-COMPANIES = ["AMZN", "MSFT", "NVDA"]
 
-downloader = Downloader("Stanford University", "anandk12@stanford.edu", "/Users/anandkrishnan/Desktop")
+entered_company = "COST"
+def set_company(company):
+    global entered_company
+    entered_company = company
 
-for company in COMPANIES:
-    downloader.get("10-K", company, after = AFTER_DATE)
+def get_company():
+    return entered_company
+
+def run():
+    downloader = Downloader("Stanford University", "anandk12@stanford.edu", "/Users/anandkrishnan/Desktop/FSIL project")
+
+    downloader.get("10-K", entered_company, after = AFTER_DATE)
