f7550d0caa539314760ff431609c07975b02beef
anandkrishnan27 <166864805+anandkrishnan27@users.noreply.github.com>
2024-04-21 16:16:39 -0700

    Update cleanup.py
    updated with comments

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,13 @@
+"""
+cleanup.py contains functions that delete all downloaded files from the local directory
+"""
 import os
 
+"""
+Function main(): removes all files containing written item 7a. in the "financial_statements" local folder 
+"""
 def main():
     folderpath = "/Users/anandkrishnan/Desktop/FSIL project/sec-edgar-filings/financial_statements"
-
     files = os.listdir(folderpath)
 
     for file in files:
